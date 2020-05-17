a14b23f1fdce5ba3016f7f2b97df1c92f1a88700
Sartaj Singh <55857498+sartaj96singh@users.noreply.github.com>
2020-05-16 22:52:41 -0700

    Upload MainFile.py
    Upload MainFile.py

---
+++ MainFile.py
@@ -0,0 +1,34 @@
+from pycoingecko import CoinGeckoAPI
+import time
+# coded by Taj and HMan
+
+# add - time when new price was added
+# add - write data out to file
+
+cg = CoinGeckoAPI()
+
+cryptoNames = ['bitcoin', 'ethereum']
+
+priceDict = {
+}
+while True:
+    for crypto in cryptoNames:
+
+        price = cg.get_price(ids=crypto, vs_currencies="usd")
+
+        usdPrice = price[crypto]['usd']
+
+        if priceDict.get(crypto):
+            priceList = priceDict.get(crypto)
+            if priceList[-1] != usdPrice:
+                priceList.append(usdPrice)
+        else:
+            newList = [usdPrice]
+            priceDict[crypto] = newList
+
+    print(priceDict)
+    time.sleep(60)
+
+
+
+
